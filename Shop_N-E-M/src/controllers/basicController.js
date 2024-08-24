bbe19fe3c8d7e5264bac0f0b3c0b19719aa5c8d5
hoanggiahuy <huypg7645@gmail.com>
2024-08-24 14:52:41 +0700

    f-index.css add-carousel-hotsale

--- Shop_N-E-M/src/controllers/basicController.js
+++ Shop_N-E-M/src/controllers/basicController.js
@@ -1,5 +1,7 @@
 export const getHomePage = (req, res) => {
+    const saleEndDate = new Date('2024-09-01T00:00:00').getTime();
     res.render('pages/Homepage', {
-        title: "Homepage"
+        title: "Homepage",
+        saleEndDate
     })
 }
